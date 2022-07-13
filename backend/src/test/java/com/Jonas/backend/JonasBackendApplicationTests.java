77b1baf1e224052a6adce4e3be64a7738bcef6e2
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-12 22:05:51 -0300

    Project created

---
+++ backend/src/test/java/com/Jonas/backend/JonasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jonas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
