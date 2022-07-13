0174998ae9c2a0e7c0f60a4c00b0f475eec27271
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-13 20:52:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>Olá Mundo</h1>
+    <div>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </div>
   )
 }
 
