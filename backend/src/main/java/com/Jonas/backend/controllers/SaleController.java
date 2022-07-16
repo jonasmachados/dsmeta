c7e9fb0183839c31efdbab7cb05415f571da352e
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-16 19:50:15 -0300

    Date select

--- backend/src/main/java/com/Jonas/backend/controllers/SaleController.java
+++ backend/src/main/java/com/Jonas/backend/controllers/SaleController.java
@@ -2,22 +2,26 @@ package com.Jonas.backend.controllers;
 
 import com.Jonas.backend.entities.Sale;
 import com.Jonas.backend.services.SaleService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "sales")
+@RequestMapping(value = "/sales")
 public class SaleController {
-    
+
     @Autowired
     private SaleService service;
-    
+
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable ) {
+        return service.findSales(minDate, maxDate, pageable);
     }
-    
 }
--- backend/src/main/java/com/Jonas/backend/repositories/SaleRepository.java
+++ backend/src/main/java/com/Jonas/backend/repositories/SaleRepository.java
@@ -1,8 +1,14 @@
 package com.Jonas.backend.repositories;
 
 import com.Jonas.backend.entities.Sale;
+import java.time.LocalDate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
