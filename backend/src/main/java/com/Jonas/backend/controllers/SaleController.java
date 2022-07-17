e3aa7677f3717b75c50c39def238c98903b288c0
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-17 09:52:58 -0300

    Set up Twilio sms

--- backend/src/main/java/com/Jonas/backend/controllers/SaleController.java
+++ backend/src/main/java/com/Jonas/backend/controllers/SaleController.java
@@ -2,10 +2,12 @@ package com.Jonas.backend.controllers;
 
 import com.Jonas.backend.entities.Sale;
 import com.Jonas.backend.services.SaleService;
+import com.Jonas.backend.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
-            Pageable pageable ) {
+            Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
