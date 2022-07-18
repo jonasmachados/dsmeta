643d7f5977f4e7a070e285628a4831d38c22c011
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-17 21:29:20 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -16,10 +16,16 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+    const dmin = minDate.toISOString().slice(0, 10);
+
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <div>
@@ -60,9 +66,11 @@ function SalesCard() {
             <tbody>
               {sales.map((sale) => {
                 return (
-                  <tr key={sale.id}>
+                  <tr>
                     <td className="show992">{sale.id}</td>
-                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                    <td className="show576">
+                      {new Date(sale.date).toLocaleDateString()}
+                    </td>
                     <td>{sale.sellerName}</td>
                     <td className="show992">{sale.visited}</td>
                     <td className="show992">{sale.deals}</td>
